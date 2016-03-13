67888fd61d1d2b99b685b1c78a74030bfdfe58c2
Lee Yanco <lee.yanco@gmail.com>
2016-03-12 21:20:29 -0500

    Refactor Capybara to be all one flow

--- test/features/user_actions_test.rb
+++ test/features/user_actions_test.rb
@@ -2,81 +2,58 @@ require "test_helper"
 
 class UserActionsTest < Capybara::Rails::TestCase
 
-  test "layout should include name and email" do
+
+  test "full feature" do
+    #layout should include name and email
     visit users_new_path
     assert_content page, "Name"
     assert_content page, "Email"
-  end
-
-  test "submitting form should create a new user" do
-    visit users_new_path
-    fill_in('Name', with: 'Test McTesterson')
-    fill_in('Email', with: 'test@example.com')
-    fill_in('Password', with: 'abcdefgh')
-    fill_in('Confirmation', with: 'abcdefgh')
-    click_link_or_button('Create my account')
-
-    assert_content page, "Welcome to the Sample App!"
-    assert_content page, 'Test McTesterson'
-  end
-
-
-  test "failing form should stay on same page" do
-    visit users_new_path
+    #When I input bad information
     fill_in('Name', with: 'Test McTesterson')
     fill_in('Email', with: 'test@example.com')
     fill_in('Password', with: 'ssssss')
     fill_in('Confirmation', with: 'abcdefgh')
     click_link_or_button('Create my account')
-
+    #Then the signup should render again
     assert current_path, users_new_path
-  end
-
-  test "visiting login should show email and password" do
-    visit login_path
-    assert_content page, "Email"
-    assert_content page, "Password"
-  end
-
-  test "I should log in and see my name" do
-    visit login_path
-    fill_in('Email', with: 'lee@example.com')
-    fill_in('Password', with: 'password')
-    click_button('Log in')
-
-    assert_content page, 'Lee Yanco'
-
-  end
-
-  test "I should be able to log out" do
-    visit root_path
-    click_link('Log in')
-    fill_in('Email', with: 'lee@example.com')
-    fill_in('Password', with: 'password')
-    click_button('Log in')
+    #When I enter good information
+    fill_in('Name', with: 'Test McTesterson')
+    fill_in('Email', with: 'test@example.com')
+    fill_in('Password', with: 'abcdefgh')
+    fill_in('Confirmation', with: 'abcdefgh')
+    click_link_or_button('Create my account')
+    #Then I should see my name and a welcome message and be logged in
+    assert_content page, "Welcome to the Sample App!"
+    assert_content page, 'Test McTesterson'
+    #When I log out
     click_link('Log out')
+    #Then I should see a prompt to log back in
     assert_content page, 'Log in'
 
-  end
+    #When I visit the login page
+    visit login_path
+    #Then I should see a prompt for email and password
+    assert_content page, "Email"
+    assert_content page, "Password"
 
-  test "I should be able to see my info edited" do
-    visit root_path
-    click_link('Log in')
+    #When I log in
+    visit login_path
     fill_in('Email', with: 'lee@example.com')
     fill_in('Password', with: 'password')
     click_button('Log in')
+    #Then I should see my name on the page
+    assert_content page, 'Lee Yanco'
+
+    #When I go to settings
     click_link('Settings')
     assert_content page, 'Update your profile'
     assert_content page, 'Confirmation'
+    #And change my password
     fill_in('Password', with: 'password')
     fill_in('Confirmation', with: 'password')
     click_button('Save changes')
+    #Then I should see a prompt that my profile has been updated
     assert_content page, 'Profile updated'
-
   end
-
-
-
-
 
 end
